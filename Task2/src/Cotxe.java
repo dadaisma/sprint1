92d241b87926b4525371f3ddea230ac2640775b1
dadaisma <infodadaisma@gmail.com>
2024-03-05 12:36:48 +0100

    task2

---
+++ Task2/src/Cotxe.java
@@ -0,0 +1,46 @@
+public class Cotxe {
+    private static final String marca = "Toyota";
+    private static String model;
+    private final int potencia;
+
+    public Cotxe(String model, int potencia) {
+        this.model = model;
+        this.potencia = potencia;
+    }
+
+    public static void frenar() {
+        System.out.println("El vehicle està frenant");
+    }
+
+    public void accelerar() {
+        System.out.println("El vehicle està accelerant");
+    }
+
+    public static String getMarca() {
+        return marca;
+    }
+
+    public static String getModel() {
+        return model;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public static void main(String[] args) {
+        // Creem una instància de Cotxe
+        Cotxe cotxe = new Cotxe("Corolla", 150);
+
+        // Mostrem la marca, el model i la potència
+        System.out.println("Marca: " + Cotxe.getMarca());
+        System.out.println("Model: " + Cotxe.getModel());
+        System.out.println("Potència: " + cotxe.getPotencia());
+
+        // Invocació del mètode estàtic frenar()
+        Cotxe.frenar();
+
+        // Invocació del mètode no estàtic accelerar()
+        cotxe.accelerar();
+    }
+}
---
+++ Task2/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create an instance of Cotxe
+        Cotxe cotxe = new Cotxe("Corolla", 150);
+
+        // Display the brand, model, and power
+        System.out.println("Marca: " + Cotxe.getMarca());
+        System.out.println("Model: " + Cotxe.getModel());
+        System.out.println("Potència: " + cotxe.getPotencia());
+
+        // Invocation of the static method frenar()
+        Cotxe.frenar();
+
+        // Invocation of the non-static method accelerar()
+        cotxe.accelerar();
+    }
+}
