bb37e1d90494eb29385e610bba74b115352e6833
dadaisma <infodadaisma@gmail.com>
2024-03-11 13:51:00 +0100

    n1task4e2fix

--- Task4/src/n1exercici2/CalculoDniTest.java
+++ Task4/src/n1exercici2/CalculoDniTest.java
@@ -1,11 +1,13 @@
 package n1exercici2;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculoDniTest {
+    private static int totalTestsRun = 0;
 
     @ParameterizedTest
     @ValueSource(ints = {
@@ -16,6 +18,7 @@ public class CalculoDniTest {
             72036403,
             27832073, 51903394, 34649105, 21505631, 84778212})
     public void testCalcularLletraDni(int numeroDni) {
+        totalTestsRun++;
         char letraEsperada;
         switch (numeroDni) {
             case 91219948:
@@ -57,5 +60,16 @@ public class CalculoDniTest {
 
         // Print the number of DNI values being tested
         System.out.println("Numero DNI: " + numeroDni + " Lletra esperada: " + letraEsperada+ " Lletra calculada " + letraCalculada );
+          }
+    // Static method to retrieve the total number of tests run
+
+    public static int getTotalTestsRun() {
+        return totalTestsRun;
+    }
+
+    // After all tests have run, print the total number of tests run
+    @AfterAll
+    public static void printTotalTestsRun() {
+        System.out.println("\n" + "Total de proves executades: " + totalTestsRun);
     }
 }
