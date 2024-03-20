6dbb847d7847265088354857494dd4b574a238df
dadaisma <infodadaisma@gmail.com>
2024-03-20 13:55:38 +0100

    n2task31bcww

--- Task3/src/n2exercici2/Main2.java
+++ Task3/src/n2exercici2/Main2.java
@@ -1,25 +1,30 @@
 package n2exercici2;
 
-import java.util.HashSet;
+import java.util.*;
 
 public class Main2 {
 
     public static void main(String[] args) {
 
-        HashSet<Restaurant2> restaurants = new HashSet<>();
+        List<Restaurant2> restaurants = new ArrayList<>();
 
-        // Afegir restaurants al HashSet
+
         restaurants.add(new Restaurant2("Restaurant Da Roberto se come bien", 4));
+        restaurants.add(new Restaurant2("Restaurant Da Roberto Antonio se come bien", 3));
         restaurants.add(new Restaurant2("Restaurant Michelin", 5));
+        restaurants.add(new Restaurant2("Restaurant Michelin", 3));
         restaurants.add(new Restaurant2("Restaurant PEppino, comida rapida y mala", 2));
 
         // No s'afegirà perquè ja existeix un restaurant amb el mateix nom i puntuació
         restaurants.add(new Restaurant2("Restaurant Da Roberto se come bien", 4));
 
+        // Ordenar la llista per nom i puntuació
+        Collections.sort(restaurants, Comparator.comparing(Restaurant2::getNom)
+                .thenComparingInt(Restaurant2::getPuntuació));
+
         // Mostrar els restaurants
         for (Restaurant2 restaurant : restaurants) {
-            System.out.println(restaurant);
+            System.out.println(restaurant.getNom() + " " + restaurant.getPuntuació());
         }
 
     }
-}
