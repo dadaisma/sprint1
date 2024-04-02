cecd1f405062f60f91c3f182147ccd4f74690ca6
dadaisma <infodadaisma@gmail.com>
2024-04-02 11:30:39 +0200

    n1task1review

--- Task1/src/n1exercici1/Main.java
+++ Task1/src/n1exercici1/Main.java
@@ -3,30 +3,30 @@ package n1exercici1;
 public class Main {
     public static void main(String[] args) {
 
-        Vent vent = new Vent("Flauta", 50.85);
+        Vent vent = new Vent("Flauta", 40.45);
         System.out.println(vent.tocar());
 
-        Vent sax = new Vent("sax", 50.85);
+        Vent sax = new Vent("sax", 57.85);
         System.out.println(sax.tocar());
 
         System.out.println(Vent.metodoStatic());
 
         System.out.println();
 
-        Corda corda = new Corda("Guitarra", 150.5);
+        Corda corda = new Corda("Guitarra", 201.5);
         System.out.println(corda.tocar());
 
-        Corda benjo = new Corda("benjo", 150.5);
+        Corda benjo = new Corda("benjo", 180.5);
         System.out.println(benjo.tocar());
 
         System.out.println(Corda.metodoStatic());
 
         System.out.println();
 
-        Perc perc = new Perc("Timbal", 210.35);
+        Perc perc = new Perc("DumDum", 270.35);
         System.out.println(perc.tocar());
 
-        Perc bongo = new Perc("bongo", 210.35);
+        Perc bongo = new Perc("bongo", 99.35);
         System.out.println(bongo.tocar());
 
         System.out.println(Perc.metodoStatic());
