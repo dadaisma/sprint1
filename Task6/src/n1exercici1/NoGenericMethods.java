88d7f7d4162bea6219b6f76a3aa620d069091bb0
dadaisma <114525596+dadaisma@users.noreply.github.com>
2024-06-06 10:19:01 +0200

    assignedatype

--- Task6/src/n1exercici1/NoGenericMethods.java
+++ Task6/src/n1exercici1/NoGenericMethods.java
@@ -1,39 +1,37 @@
-package n1exercici1;
+public class NoGenericMethods <String> {
 
-public class NoGenericMethods <T> {
+    private String object1;
+    private String object2;
+    private String object3;
 
-    private T object1;
-    private T object2;
-    private T object3;
-
-    public NoGenericMethods(T obj1, T obj2, T obj3){
+    public NoGenericMethods(String obj1, String obj2, String obj3){
         this.object1 = obj1;
         this.object2 = obj2;
         this.object3 = obj3;
 
     }
 
-    public void setObject1(T object1) {
+    public void setObject1(String object1) {
         this.object1 = object1;
     }
 
-    public T getObject1() {
+    public String getObject1() {
         return object1;
     }
 
-    public void setObject2(T object2) {
+    public void setObject2(String object2) {
         this.object2 = object2;
     }
 
-    public T getObject2() {
+    public String getObject2() {
         return object2;
     }
 
-    public void setObject3(T object3) {
+    public void setObject3(String object3) {
         this.object3 = object3;
     }
 
-    public T getObject3() {
+    public String getObject3() {
         return object3;
     }
 }
